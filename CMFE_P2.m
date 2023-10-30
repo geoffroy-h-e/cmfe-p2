0bdb6d677b812c6f56a470c1a27f4b927d054913
geoffroy-h-e <geoffroy.hebert-emond@hec.ca>
2023-10-30 18:25:42 -0400

    changes

--- CMFE_P2.m
+++ CMFE_P2.m
@@ -6,22 +6,38 @@ Phi = [0.7146 0; 0 0.0353];
 Psi = [0.0378 0; 0 0.0947]; 
 Sigma = 0.0428; % var e t 
 Lamda = 2.14*10^-5; % cost of transaction on a typical 1k shares
-I = [1 0; 0 1]; %identity matrix
-U = 0 % do we need to initialize u? 
+I = eye(2); %identity matrix
+%U = 0; % do we need to initialize u? 
 
 f0_mean = [0 0];
 Omega0= [0.0412 0; 0 1.3655]; %cov of fzero 
 f0_sampled = mvnrnd(f0_mean, Omega0, 1);% sample f0 to initialize code 
 
-%deterministic policy constraints
+% Set the optimization options
+options = optimoptions('fmincon', 'Display', 'iter', 'Algorithm', 'sqp');
+
+% Define the anonymous function for optimization
+objFun = @(u) objective_function(u, X0, B, Phi, f0_sampled, Lamda);
+
+% Set initial guess for u
+u0 = zeros(T, 1);
+
+% Equality and inequality constraints
 Aeq = zeros(T, T);
 beq = zeros(T, 1);
-
 for t = 2:T
     Aeq(t, t-1:t) = [1, -1];
 end
-
 Aeq(1,1) = 1;
 beq(1) = X0;
-Aineq = [eye(T); -eye(T)];
-bineq = zeros(2*T, 1);
+Aineq = [];
+bineq = [];
+
+% Call optimization solver
+[u_opt, obj_val] = fmincon(objFun, u0, Aineq, bineq, Aeq, beq, [], [], [], options);
+
+% Display results
+disp('Optimal u:');
+disp(u_opt);
+disp('Objective function value at optimal u:');
+disp(obj_val);
--- objective_function.m
+++ objective_function.m
@@ -1,10 +1,16 @@
 %deterministic policy function
-function obj = objective_function(u, X0, B, Phi, f0_sampled, Sigma, I, Lamda)
+
+function obj = objective_function(u, X0, B, Phi, f0_sampled, Lamda, I)
+    
     T = length(u);
+
+    X = zeros(T, 1);
+    X(1) = X0;
+
     sum_val = 0;
     for t = 1:T
-        x_t = X(t);
-        sum_val = sum_val + (X0' * B * ((I - Phi)^2) * f0_sampled) - (0.5 * u' * Lamda * u);
+        X(t+1) = X(t) - u(t);
+        sum_val = sum_val + (X(t)' * B * ((I - Phi)^t) * f0_sampled) - (0.5 * u(t)' * Lamda * u(t));
     end
     obj = -sum_val; % Negative since we want to maximize
 end
