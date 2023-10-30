c586b35d0f4ff24788deceb23383617d493dea7a
geoffroy-h-e <geoffroy.hebert-emond@hec.ca>
2023-10-30 19:03:08 -0400

    test1

--- CMFE_P2.m
+++ CMFE_P2.m
@@ -2,7 +2,7 @@
 T = 12; % time interval
 X0 = 100000; % initial number of shares 
 
-B = [0.3375 -0.072]; %factor loading matrix
+B = [0.3375; -0.072]; % factor loading matrix adjusted to 2x1
 
 Phi = [0.7146 0; 0 0.0353]; 
 Psi = [0.0378 0; 0 0.0947]; 
@@ -10,9 +10,9 @@ Sigma = 0.0428; % var e t
 Lamda = 2.14*10^-5; % cost of transaction on a typical 1k shares
 I = eye(2); % identity matrix
 
-f0_mean = [0 0];
+f0_mean = [0; 0];
 Omega0 = [0.0412 0; 0 1.3655]; % cov of fzero 
-f0_sampled = mvnrnd(f0_mean, Omega0, 1); % sample f0 to initialize code 
+f0_sampled = mvnrnd(f0_mean, Omega0, 1)'; % sampled f0 as column vector 
 
 % Set the optimization options
 options = optimoptions('fmincon', 'Display', 'iter', 'Algorithm', 'sqp');
